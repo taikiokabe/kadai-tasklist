984cfdebd344ab249160b315f448c8a05c9dea68
taikiokabe <tokabe87@gmail.com>
2017-11-03 10:02:59 +0000

    Add status column to tasklists

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029123325) do
+ActiveRecord::Schema.define(version: 20171103100153) do
 
   create_table "tasklists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
     t.index ["user_id"], name: "index_tasklists_on_user_id", using: :btree
   end
 
